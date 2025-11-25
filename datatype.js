ab5e1d8984cf2a6692df57374d466bbf980c69b7
pradeepit2491-a11y <pradeepit2491@gmail.com>
2025-11-25 21:10:26 +0530

    Add files via upload

---
+++ datatype.js
@@ -0,0 +1,20 @@
+//First name
+let firstName="Pradeep" 
+console.log(firstName)
+ console.log(typeof firstName)
+ //company name 
+ let companyName= null 
+ console.log(companyName)
+  console.log(typeof companyName) 
+  //mobile no
+  let mobileNumber=9994456756 
+  console.log(mobileNumber) 
+  console.log(typeof mobileNumber)
+  //isautomation 
+  let isAutomation=true 
+  console.log(isAutomation) 
+  console.log(typeof isAutomation) 
+  //Do not assign
+  let hasAutomation 
+  console.log(hasAutomation)
+   console.log(typeof hasAutomation)
